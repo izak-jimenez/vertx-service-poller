7710e0f9d06af0a2a9b7b5655adc9f8be6d7649d
Isaac Jimenez <izakjimenez@gmail.com>
2021-11-22 16:52:10 -0600

    Modified setup-server.sh env variables export

--- setup-server.sh
+++ setup-server.sh
@@ -1,8 +1,8 @@
 # Run this bash file to setup the application in Unix
 
 # shellcheck disable=SC2034
-MONGODB_URI="mongodb+srv://kry:JBE7E3hBtgkRrSoL@kry-cluster.ovhky.mongodb.net/krydb?retryWrites=true&w=majority"
-MONGODB_DATABASE="krydb"
+export MONGODB_URI="mongodb+srv://kry:JBE7E3hBtgkRrSoL@kry-cluster.ovhky.mongodb.net/krydb?retryWrites=true&w=majority"
+export MONGODB_DATABASE="krydb"
 
 echo "Building Vert.x REST API"
 ./gradlew clean build
